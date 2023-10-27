1e3ebf396396040ee7bf869f321e4977be9242b5
Mikdan Jey <mikdanjey@gmail.com>
2023-10-27 21:23:22 +0530

    ok

--- db/config.py
+++ db/config.py
@@ -1,10 +1,22 @@
+import os
 from pydantic_settings import BaseSettings
 
+from dotenv import load_dotenv
+
+load_dotenv()
+
+MONGO_URI = os.getenv("MONGO_URI")
+
+print(os.getenv("MONGO_URI"))
+
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+HOST_URL = os.getenv("HOST_URL")
 
 class Settings(BaseSettings):
-    mongo_uri
+    mongo_uri = MONGO_URI
     git_rev = "beta"
-    secret_key
+    secret_key = SECRET_KEY
     host_url = "https://media-fusion.vercel.app" # "https://882b9915d0fe-mediafusion.baby-beamup.club"
     logging_level = "INFO"
 
